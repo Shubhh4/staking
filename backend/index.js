44d6c7db2e57e3ab68d553ab81a43402c2202e5f
Shubham's <sk192196@gmail.com>
2023-06-12 00:22:42 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import Moralis from 'moralis';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+const app = express();
+const port = 5001;
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+const MORALIS_API_KEY = process.env.MORALIS_API_KEY;
+
+app.get("/getwalletbalance", async(res,req)=>{
+    try{
+        const{query} = req;
+        const response = await Moralis.EvmApi.balance.getNativeBalance({
+            chain: "0xaa36a7",
+            address: query.address,
+
+        });
+        return res.status(200).json(response);
+    } catch(e){
+        console.log(`Something went wrong ${e}`)
+        return res.status(400).json();
+    }
+})
+
+//our endpoint
+Moralis.start({
+    apiKey: MORALIS_API_KEY
+}).then(()=>{
+    app.listen(port, ()=>{
+       console.log("Listening for API Calls"); 
+    });
+})
+
