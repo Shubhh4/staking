f5df2b00e96ec305a74bf4d65362d7e06ecff5d7
Shubham Pathak <95904081+Shubhh4@users.noreply.github.com>
2023-06-12 22:39:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import Moralis from 'moralis';
 import dotenv from 'dotenv';
-import cors from 'corss';
+import cors from 'cors';
 
 const app = express();
 const port = 5001;
